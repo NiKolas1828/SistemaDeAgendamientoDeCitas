6a779983a5d3af6e89c496a3ea4358e29538400b
Nicolas Rodriguez <nicolas.rodriguez.romero@correounivalle.edu.co>
2025-12-18 13:16:56 -0500

    feat: inital project structure

---
+++ src/main/java/com/example/sistemaagendamientocitas/SistemaAgendamientoCitasApplication.java
@@ -0,0 +1,13 @@
+package com.example.sistemaagendamientocitas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaAgendamientoCitasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SistemaAgendamientoCitasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sistemaagendamientocitas/SistemaAgendamientoCitasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sistemaagendamientocitas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaAgendamientoCitasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
